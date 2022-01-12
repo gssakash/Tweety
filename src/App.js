716d5e4298997a86cea2043a51b70eb683bcbab8
Akash <gsaisriram.akash@gmail.com>
2022-01-12 09:05:40 +0530

    Footer Added and Restructuring finished

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import AccountButton from "./components/AccountButton";
 
 function App(props) {
-
-  const [web3, setWeb3] = useState()
-  const [alephAccount, setAlephAccount] = useState()
-  const [account, setAccount] = useState()
+  const [web3, setWeb3] = useState();
+  const [alephAccount, setAlephAccount] = useState();
+  const [walletAddress, setWalletAddress] = useState();
 
   const connectWallet = async (e) => {
-    const { alephAccount, web3 } = await props.connectWeb3(e)
-    const accounts = await web3.eth.getAccounts()
+    const { alephAccount, web3 } = await props.connectWeb3(e);
+    const accounts = await web3.eth.getAccounts();
 
-    setWeb3(web3)
-    setAlephAccount(alephAccount)
-    setAccount(accounts[0])
-  }
+    setWeb3(web3);
+    setAlephAccount(alephAccount);
+    setWalletAddress(accounts[0]);
+  };
 
+  //The account data stays saved even after refresh.
   useEffect(() => {
     if (window.ethereum.isConnected()) {
-      connectWallet()
+      connectWallet();
     }
-  }, [])
+  }, []);
 
   return (
     <div className="App">
-      <Navbar connectWallet={connectWallet} account={account} />
+      <Navbar />
+      <AccountButton
+        connectWallet={connectWallet}
+        walletAddress={walletAddress}
+      />
+      <div className="container">
+        <div className="row">
+          <div className="col-9 offset-3">
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/AccountButton.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function AccountButton(props) {
+  return (
+    <nav className="navbar fixed-bottom">
+      <div className="container">
+        <div className="col-3">
+          {props.walletAddress ? (
+            props.walletAddress
+          ) : (
+            <a
+              className="btn btn-primary"
+              aria-current="page"
+              href="/"
+              onClick={props.connectWallet}
+            >
+              Connect Wallet
+            </a>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default AccountButton;
